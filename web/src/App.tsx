9264398e4f5972709f65697bb4e50d57aec53afd
Jordan Ferreira <jordanferreira69@gmail.com>
2022-09-20 23:08:52 -0300

    update web ad bar

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,3 +1,5 @@
+import { MagnifyingGlassPlus } from 'phosphor-react'
+
 import './styles/main.css'
 
 import logoImg from './assets/logo-nlw-esports.svg'
@@ -62,9 +64,16 @@ function App() {
         </a>
       </div>
 
-      <div className='pt-1 bg-nlw-gradient self-stretch rounded-lg'>
-        <div className='bg-[#2A2634] px-8 py-6 mt-8'>
-
+      <div className='pt-1 mt-8 bg-nlw-gradient self-stretch rounded-lg overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 flex justify-between items-center'>
+          <div>
+            <strong className='text-2xl text-white font-black block'>Não encontrou seu duo?</strong>
+            <span className='text-zinc-400 block'>Publique um anúncio para encontrar novos players!</span>
+          </div>
+          <button className='px-4 py-3 bg-violet-500 hover:bg-violet-600 text-white rounded-md flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
         </div>
       </div>
     </div>
